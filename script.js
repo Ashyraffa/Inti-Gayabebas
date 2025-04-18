68ab2e620d1a87844c7c84bf7fee9b286ccf24dc
Ashyraffa3527 <raffadandilla@gmail.com>
2025-04-18 20:31:56 +0700

    bug fix

--- script.js
+++ script.js
@@ -76,3 +76,12 @@ function toggleContent(contentId) {
         showSlide(currentIndex);
 
 
+// Jam 
+function updateClock () {
+    const now = new Date();
+    const time = now.toLocaleTimeString();
+    document.getElementById("clock").textContent = time;
+}
+
+setInterval(updateClock, 1000);
+updateClock;
